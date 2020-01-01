62a84a938fa63a5e3884ea59f3064413528eb2b6
Imane AGNAOU <36973653+imaneamiri@users.noreply.github.com>
2020-01-01 16:59:27 +0100

    Update ProductController.java

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -19,22 +19,33 @@ import java.net.URI;
 import java.util.List;
 
 
-@Api( description="API pour es opérations CRUD sur les produits.")
 
+@Api(value = "/Produits", description="API pour les opérations CRUD sur les produits.")
 @RestController
 public class ProductController {
 
     @Autowired
-    private ProductDao productDao;
+    private ProductService productService;
 
 
     //Récupérer la liste des produits
 
     @RequestMapping(value = "/Produits", method = RequestMethod.GET)
-
+    @ApiOperation(
+            value = "Récupère la liste de tous les produits.",
+            notes = "Attention, la récupération de la liste des produits est soumise à des droits d'accès.",
+            response = Product.class,
+            responseContainer = "List"
+    )
+    @ApiResponses( value  = {
+            @ApiResponse ( code = 200, message = "La liste des produits a été retrouvé avec succès."),
+            @ApiResponse( code = 401, message = "La consultation de la liste des produits n'est pas autorisé."),
+            @ApiResponse( code = 403, message = "La consultation de la liste des produits est interdite."),
+            @ApiResponse( code = 404, message  = "La liste des produits est introuvable.")
+    })
     public MappingJacksonValue listeProduits() {
 
-        Iterable<Product> produits = productDao.findAll();
+        Iterable<Product> produits = this.productService.getProductDao().findAll();
 
         SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
 
@@ -49,12 +60,21 @@ public class ProductController {
 
 
     //Récupérer un produit par son Id
-    @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
     @GetMapping(value = "/Produits/{id}")
+    @ApiOperation(
+            value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!",
+            notes = "Attention, la récupération du produit est soumise à des droits d'accès.",
+            response = Product.class
+    )
+    @ApiResponses( value  = {
+            @ApiResponse ( code  =  200, message  =  "La récupération du produit  a été effectuée avec succès."),
+            @ApiResponse( code  =  401 , message  =  "La récupération du produit est non autorisée" ),
+            @ApiResponse( code  =  403 , message  =  " La récupération du produit  est interdite." ),
+            @ApiResponse( code  =  404 , message  =  " Le produit est introuvable.")
+    })
+    public Product afficherUnProduit(@ApiParam(value = "L'id du produit à afficher.", required = true) @PathVariable int id) {
 
-    public Product afficherUnProduit(@PathVariable int id) {
-
-        Product produit = productDao.findById(id);
+        Product produit = this.productService.getProductDao().findById(id);
 
         if(produit==null) throw new ProduitIntrouvableException("Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
 
@@ -66,10 +86,28 @@ public class ProductController {
 
     //ajouter un produit
     @PostMapping(value = "/Produits")
+    @ApiOperation(
+            value = "Ajoute un produit .",
+            notes = "Attention, l'ajout d'un produit est soumis à des droits d autorisation.",
+            response = Product.class
 
-    public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
+    )
+    @ApiResponses( value  = {
+            @ApiResponse ( code  =  200, message  =  "L'ajout du produit  a été effectuée avec succès."),
+            @ApiResponse( code  =  201 , message  =  "Le produit est créé."),
+            @ApiResponse( code  =  401 , message  =  "L'ajout du produit est non autorisé" ),
+            @ApiResponse( code  =  403 , message  =  "L'ajout du produit est interdit." ),
+            @ApiResponse( code  =  404 , message  =  " Le produit est introuvable ." )
+    })
+    public ResponseEntity<Product> ajouterProduit(
+            @Valid
+            @RequestBody
+            @ApiParam(value = "Le produit à ajouter.", required = true) Product product) {
+        Product productAdded =  this.productService.getProductDao().save(product);
 
-        Product productAdded =  productDao.save(product);
+        if (product.getPrix() == 0) {
+            throw new ProduitGratuitException();
+        }
 
         if (productAdded == null)
             return ResponseEntity.noContent().build();
@@ -80,19 +118,44 @@ public class ProductController {
                 .buildAndExpand(productAdded.getId())
                 .toUri();
 
-        return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(productAdded);
     }
 
     @DeleteMapping (value = "/Produits/{id}")
-    public void supprimerProduit(@PathVariable int id) {
+    @ApiOperation (
+            httpMethod  =  "DELETE" ,
+            value  =  "Supprime un produit.")
+    @ApiResponses( value  = {
+            @ApiResponse ( code  =  200 , message  =  "La suprression a été effectuée avec succès." ),
+            @ApiResponse( code  =  204 , message  =  "Le produit a supprimer est introuvable."),
+            @ApiResponse( code  =  401 , message  =  "Droit insuffisant pour supprimer ce produit." ),
+            @ApiResponse( code  =  403 , message  =  "La suppression de ce produit est interdite." )
+    })
+    public void supprimerProduit(@ApiParam(value = "L'id du produit à supprimer.", required = true) @PathVariable int id) {
 
-        productDao.delete(id);
+        this.productService.getProductDao().delete(id);
     }
 
     @PutMapping (value = "/Produits")
-    public void updateProduit(@RequestBody Product product) {
-
-        productDao.save(product);
+    @ApiOperation(
+            value = "Fait la mise à jour du produit.",
+            notes = "Attention, la mise à jour  est soumise à des droits d'accès.",
+            response = Product.class
+    )
+    @ApiResponses( value  = {
+            @ApiResponse ( code  =  200 , message  =  "La mise à jour du produit  a été effectuée avec succès." ),
+            @ApiResponse( code  =  201 , message  =  "Le produit a été créé."),
+            @ApiResponse( code  =  401 , message  =  "la mise à jour n'est pas autorisée." ),
+            @ApiResponse( code  =  403 , message  =  "La mise à jour du produit  est interdite." ),
+            @ApiResponse( code  =  404 , message  =  "Le produit est introuvable ." )
+    })
+    public void updateProduit(
+            @RequestBody
+            @ApiParam(value = "Le produit à mettre à jour.", required = true) Product product) {
+        if (product.getPrix() == 0) {
+            throw new ProduitGratuitException();
+        }
+        this.productService.getProductDao().save(product);
     }
 
 
@@ -100,9 +163,31 @@ public class ProductController {
     @GetMapping(value = "test/produits/{prix}")
     public List<Product>  testeDeRequetes(@PathVariable int prix) {
 
-        return productDao.chercherUnProduitCher(400);
+        return this.productService.getProductDao().chercherUnProduitCher(400);
     }
 
+    @GetMapping(value = "/AdminProduits")
+    public HashMap<String,Integer> afficherMargeProduits() {
+        return this.productService.calculerMargeProduit();
+    }
+
+    @GetMapping(value = "/ProduitsAlphabetique")
+    @ApiOperation(
+            value = "Trie les produits par ordre alphabétique.",
+            notes = "Attention, le tri des produits est soumis à des droits d'accès.",
+            response = Product.class,
+            responseContainer = "List"
+    )
+    @ApiResponses( value  = {
+            @ApiResponse ( code  =  200, message  =  "Le tri des produits  par ordre alphabéthique   a été effectuée avec succès."),
+            @ApiResponse( code  =  401 , message  =  "Le tri des produits par ordre alphabétique est non autorisé" ),
+            @ApiResponse( code  =  403 , message  =  " Le tri des produits par ordre alphabétique est interdit." ),
+            @ApiResponse( code  =  404 , message  =  " Les produits à trier ne sont pas trouvés." )
+    })
+    public List<Product> trierParOrdreAlphabetique() {
+        //productDao.findAll(new Sort(Sort.Direction.ASC,nom);
+        return this.productService.getProductDao().findAllAlphabetically();
+    }
 
 
 }
